91a1104e4f75bcf5255dc64091e18839c3265569
NastyaYevdokimova <55975406+NastyaYevdokimova@users.noreply.github.com>
2021-06-03 15:08:41 +0300

    Create index.php
    Add bootstrap page

---
+++ index.php
@@ -0,0 +1,22 @@
+<nav class="navbar navbar-light bg-light">
+    <a href="#" class="navbar-brand">My portfolio</a>
+</nav>
+<ul class="navbar-nav">
+  <li class="navbar-item">
+    <a href="#" class="nav-link">Homepage</a>
+  </li>
+  <li class="navbar-item">
+    <a href="#" class="nav-link">Blog</a>
+  </li>
+  <li class="navbar-item">
+    <a href="#" class="nav-link">About</a>
+  </li>
+  <li class="navbar-item">
+    <a href="#" class="nav-link">Contact Us</a>
+  </li>
+</ul>
+<div class="jumbotron jumbotron-fluid">
+  <div class="container">
+  <h1 class="display-3">Welcome to my website</h1>
+  <p class="lead">I'm a developer and designer. Check my portfolio below</p>
+</div>
