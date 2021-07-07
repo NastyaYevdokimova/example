0f7e6ad98eac5ffbc3de98cd743bf8bc999396d6
Nastya <nastya.evdokim.99@gmail.com>
2021-07-07 14:25:22 +0300

    Create branch composer

--- www/index.php
+++ www/index.php
@@ -2,6 +2,18 @@
 include 'header.php';
 ?>
 
+<?php
+require __DIR__ . '/vendor/autoload.php';
+
+$log = new Monolog\Logger('name');
+$log->pushHandler(new Monolog\Handler\StreamHandler(
+	'app.log',
+	Monolog\Logger::WARNING
+));
+$log->addWarning('Foo')
+
+?>
+
 <nav class="navbar navbar-light bg-light">
     <a href="#" class="navbar-brand">My portfolio</a>
 </nav>
